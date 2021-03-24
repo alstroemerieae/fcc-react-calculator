a1b2795529d00dc55cba2a7e6ca630188f517aba
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-03-24 08:46:24 -0300

    Update app style

--- src/App.js
+++ src/App.js
@@ -76,33 +76,32 @@ function App() {
   return (
     <div className="App">
       <div className="calculator">
+        {/* Display */}
         <div id="display" className="calculator-display">
           {
             currentInput.length === 0 ? 0 : currentInput
           }
         </div>
-        <div className="calculator-buttons">
-          {/* Numbers */}
-          <button id="zero"     className="calculator-button" onClick={(e) => numberInput(e)}>0</button>
-          <button id="one"      className="calculator-button" onClick={(e) => numberInput(e)}>1</button>
-          <button id="two"      className="calculator-button" onClick={(e) => numberInput(e)}>2</button>
-          <button id="three"    className="calculator-button" onClick={(e) => numberInput(e)}>3</button>
-          <button id="four"     className="calculator-button" onClick={(e) => numberInput(e)}>4</button>
-          <button id="five"     className="calculator-button" onClick={(e) => numberInput(e)}>5</button>
-          <button id="six"      className="calculator-button" onClick={(e) => numberInput(e)}>6</button>
-          <button id="seven"    className="calculator-button" onClick={(e) => numberInput(e)}>7</button>
-          <button id="eight"    className="calculator-button" onClick={(e) => numberInput(e)}>8</button>
-          <button id="nine"     className="calculator-button" onClick={(e) => numberInput(e)}>9</button>
-          {/* Operations */}
-          <button id="equals"   className="calculator-button" onClick={calculateInput}>=</button>
-          <button id="add"      className="calculator-button" onClick={(e) => numberInput(e)}>+</button>
-          <button id="subtract" className="calculator-button" onClick={(e) => numberInput(e)}>-</button>
-          <button id="multiply" className="calculator-button" onClick={(e) => numberInput(e)}>*</button>
-          <button id="divide"   className="calculator-button" onClick={(e) => numberInput(e)}>/</button>
-          <button id="decimal"  className="calculator-button" onClick={(e) => numberInput(e)}>.</button>
-          <button id="clear"    className="calculator-button" onClick={clearInput}>Clear</button>
-          <button id="delete"   className="calculator-button" onClick={(e) => numberInput(e)}>Delete</button>
-        </div>
+        {/* Numbers */}
+        <button id="zero"     className="calculator-button" onClick={(e) => numberInput(e)}>0</button>
+        <button id="one"      className="calculator-button" onClick={(e) => numberInput(e)}>1</button>
+        <button id="two"      className="calculator-button" onClick={(e) => numberInput(e)}>2</button>
+        <button id="three"    className="calculator-button" onClick={(e) => numberInput(e)}>3</button>
+        <button id="four"     className="calculator-button" onClick={(e) => numberInput(e)}>4</button>
+        <button id="five"     className="calculator-button" onClick={(e) => numberInput(e)}>5</button>
+        <button id="six"      className="calculator-button" onClick={(e) => numberInput(e)}>6</button>
+        <button id="seven"    className="calculator-button" onClick={(e) => numberInput(e)}>7</button>
+        <button id="eight"    className="calculator-button" onClick={(e) => numberInput(e)}>8</button>
+        <button id="nine"     className="calculator-button" onClick={(e) => numberInput(e)}>9</button>
+        {/* Operations */}
+        <button id="equals"   className="calculator-button" onClick={calculateInput}>=</button>
+        <button id="add"      className="calculator-button" onClick={(e) => numberInput(e)}>+</button>
+        <button id="subtract" className="calculator-button" onClick={(e) => numberInput(e)}>-</button>
+        <button id="multiply" className="calculator-button" onClick={(e) => numberInput(e)}>*</button>
+        <button id="divide"   className="calculator-button" onClick={(e) => numberInput(e)}>/</button>
+        <button id="decimal"  className="calculator-button" onClick={(e) => numberInput(e)}>.</button>
+        <button id="clear"    className="calculator-button" onClick={clearInput}>Clear</button>
+        <button id="delete"   className="calculator-button" onClick={(e) => numberInput(e)}>Delete</button>
       </div>
     </div>
   );
